df566986df3c427cac548d23aebe69282c5da825
unknown <mahmood1995ahmed@gmail.com>
2023-12-05 12:28:32 +0300

    fix issue: #25 created suggestions model schema

---
+++ controllers/suggestions.js
@@ -0,0 +1,70 @@
+const Suggestion = require("../models/suggestion");
+const Major = require("../models/major");
+const Branch = require("../models/branch");
+
+const cloudinary = require("../utils/cloudinary");
+
+module.exports = {
+  index,
+  create,
+};
+
+async function index(req, res) {
+  const categories = await Major.find({});
+  const cities = await Branch.find({});
+  res.render("suggestions/index", {
+    title: "Suggest",
+    cities,
+    categories,
+    errorMessages: req.flash("Error"),
+    successMessages: req.flash("success"),
+  });
+}
+
+async function create(req, res) {
+  try {
+    // Validate contact number
+    const contactRegex = /^(3)\d{7}$|(17|80|66|69)\d{6}$/;
+    const contact = req.body.contact;
+
+    //Validate Google Embeded link
+    const googleMapRegex =
+      /<iframe\s*src="https:\/\/www\.google\.com\/maps\/embed\?[^"]+"*\s*[^>]+>*<\/iframe>/;
+    const googleMap = req.body.location;
+
+    //Validate Event name
+    const eventNameRegex = /^(?=(.*[a-zA-Z]){3})[a-zA-Z0-9\s]+$/;
+    const eventName = req.body.name;
+
+    //Validate Cost
+    const eventCostRegex = /^\d{1,7}(\.\d{1,2})?$/;
+    const eventCost = req.body.cost;
+
+    if (!contactRegex.test(contact) && contact == !null) {
+      req.flash("Error", `Invalid Contact Number!`);
+      res.redirect("/suggestions/");
+    } else if (!googleMapRegex.test(googleMap)) {
+      req.flash("Error", `Invalid Google Maps Link`);
+      res.redirect("/suggestions/");
+    } else if (!eventNameRegex.test(eventName)) {
+      req.flash("Error", `Invalid Event Name`);
+      res.redirect("/suggestions/");
+    } else if (!eventCostRegex.test(eventCost) && eventCost == !null) {
+      req.flash("Error", `Invalid Cost`);
+      res.redirect("/suggestions/");
+    } else {
+      // Upload image to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+      req.body.profile_img = result.secure_url;
+      req.body.cloudinary_id = result.public_id;
+      req.body.user = req.user._id;
+
+      await Suggestion.create(req.body);
+      req.flash("success", `Suggest Added successfully!`);
+      res.redirect("/suggestions/");
+    }
+  } catch (error) {
+    req.flash("Error", `Failed Suggest!`);
+    res.redirect("/suggestions/");
+  }
+}
---
+++ models/suggestion.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const suggestionSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+  },
+  majorId: {
+    type: Schema.Types.ObjectId,
+    ref: "major",
+    required: true,
+  },
+  cost: {
+    type: String,
+    default: "Not specified",
+  },
+  startDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  endDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  timeDuration: {
+    type: String,
+    default: "Not specified",
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  cityId: {
+    type: Schema.Types.String,
+    ref: "city",
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    default: false,
+  },
+  profile_img: String,
+  cloudinary_id: String,
+},
+{
+  timestamps: true,
+});
+
+module.exports = mongoose.model("Suggestion", suggestionSchema);
